798f5fe2aa04852df1b8f1a652bd901855a0b9ed
mvmmtbhau <nguyen1hau2211@gmail.com>
2023-02-15 14:54:03 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const contactRouter = require('./app/routes/contact.route');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactRouter);
 
 app.get('/', (req, res) => {
     res.json({
